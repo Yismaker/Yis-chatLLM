9917c118f7ac68e7ef6fcfa48344c3a6bdd5e609
Yismaker <1922251193@qq.com>
2026-03-03 20:26:44 +0800

    实现流式输出

--- src/main/java/com/Yis/java/ai/langchain4j/assistant/YisAgent.java
+++ src/main/java/com/Yis/java/ai/langchain4j/assistant/YisAgent.java
@@ -4,17 +4,19 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
+import reactor.core.publisher.Flux;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 
 @AiService(
         wiringMode = EXPLICIT,
-        chatModel = "qwenChatModel",
+//        chatModel = "qwenChatModel",
+        streamingChatModel = "qwenStreamingChatModel",
         chatMemoryProvider = "Yis_chatMemoryProvider",
         tools = "appointmentTools",
         contentRetriever = "contentRetrieverYis"
 )
 public interface YisAgent {
     @SystemMessage(fromResource = "yis-prompt-template.txt")
-    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+    Flux<String> chat(@MemoryId Long memoryId, @UserMessage String userMessage);
 }
--- src/main/java/com/Yis/java/ai/langchain4j/controller/YisController.java
+++ src/main/java/com/Yis/java/ai/langchain4j/controller/YisController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @Tag(name = "YisController", description = "YisController")
 @RestController
@@ -20,8 +21,12 @@ public class YisController {
     private YisAgent yisAgent;
 
     @Operation(summary = "对话")
-    @PostMapping("/chat")
-    public String chat(@RequestBody ChatForm chatForm){
+//    @PostMapping("/chat")
+//    public String chat(@RequestBody ChatForm chatForm){
+//        return yisAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
+//    }
+    @PostMapping(value = "/chat", produces = "text/stream;charset=utf-8")
+    public Flux<String> chat(@RequestBody ChatForm chatForm) {
         return yisAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
     }
 }
